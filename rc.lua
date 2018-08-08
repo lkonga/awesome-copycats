216fc04b3fe7aa5d3b9a55d344607ba6443b35cc
conga <cmsolution@gmail.com>
2018-08-09 03:10:40 +0700

    changes

--- rc.lua
+++ rc.lua
@@ -109,7 +109,7 @@ awful.layout.layouts = {
 	--lain.layout.termfair.center,
 	awful.layout.suit.floating,
 	awful.layout.suit.tile,
-	awful.layout.suit.tile.left,
+	awful.layout.suit.floating,
 	awful.layout.suit.tile.bottom,
 	awful.layout.suit.tile.top,
 	--awful.layout.suit.fair,
